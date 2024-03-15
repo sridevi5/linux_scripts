44ab365a6e1c9522e41e61fe9afdac51aa332aef
Sridevi <69251556+sridevi5@users.noreply.github.com>
2024-03-15 23:28:18 +0530

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "Enter the year : " y
-r1=`expr $y % 400'
-r2=`expr $y % 100'
-r3=`expr $y % 4'
+r1=`expr $y % 400`
+r2=`expr $y % 100`
+r3=`expr $y % 4`
 if [ $r3 -eq 0 ] && [ $r2 -ne 0 ] || [ $r1 -eq 0 ]
 then
 echo "Given year is leap year"
